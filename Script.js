d07bbef958bd469e09a896ba0288e586cafcc356
AdrianKuijpers <302659507@student.rocmondriaan.nl>
2023-09-15 15:10:28 +0200

    hogerLager V3
    Main game working.

--- Script.js
+++ Script.js
@@ -1,11 +1,20 @@
+// prompt voor de naam van Speler 1
+const button1 = document.querySelector( ".Speler1");
+    const owntext = document.querySelector(".owntext");
+    let promptnaam = prompt("geef je naam!");
+if (promptnaam !=null);
+owntext.textContent = promptnaam;
+
+//uitkomst
 let uitkomsten = {
     speler1: 0,
     speler2: 0
-  };
-  
-  function gooiDobbelsteen(speler) {
+};
+
+//dobbelstenen met hun functies
+function gooiDobbelsteen(speler) {
     let uitkomst = Math.floor(Math.random() * 20) + 1;
-  
+
     if (speler === 1) {
         uitkomsten.speler1 = uitkomst;
         document.getElementById("uitkomst1").innerHTML = "Speler 1: " + uitkomst;
@@ -13,35 +22,36 @@ let uitkomsten = {
         uitkomsten.speler2 = uitkomst;
         document.getElementById("uitkomst2").innerHTML = "Speler 2: " + uitkomst;
     }
-  }
-  
-  function Hogerer() {
+}
+
+//uitkomst wanneer hoger
+function Hogerer() {
     let hoger = "";
- 
 
     if (uitkomsten.speler1 > uitkomsten.speler2) {
-        hoger = "Speler 1 heeft gewonnen!";
+        hoger = alert("Speler 1 heeft gewonnen!");
     } else if (uitkomsten.speler1 < uitkomsten.speler2) {
-        hoger = "Speler 2 heeft gewonnen!";
+        hoger = alert("Speler 2 heeft gewonnen!");
     } else {
-        hoger = "Het is gelijkspel!";
+        hoger = alert("Het is gelijkspel!");
     }
-  
+
     document.getElementById("lager").innerHTML = hoger;
 
-}   
+}
 
+//uitkomst wanneer lager
 function Laagerer() {
-  let lager = "";
+    let lager = "";
 
-  if (uitkomsten.speler1 < uitkomsten.speler2) {
-   lager = "Speler 1 heeft gewonnen!";
-  } else if (uitkomsten.speler1 > uitkomsten.speler2) {
-      lager = "Speler 2 heeft gewonnen!";
-  } else {
-      lager = "Het is gelijkspel!";
-  }
+    if (uitkomsten.speler1 < uitkomsten.speler2) {
+        lager = alert("Speler 1 heeft gewonnen!");
+    } else if (uitkomsten.speler1 > uitkomsten.speler2) {
+        lager = alert("Speler 2 heeft gewonnen!");
+    } else {
+        lager = alert("Het is gelijkspel!");
+    }
 
-  document.getElementById("lager").innerHTML = lager;
+    document.getElementById("lager").innerHTML = lager;
 
 }
